a3f10615896f4639233c7e4e475b644ca2b7ddbc
Dipta-b <diptabanik0@gmail.com>
2025-08-31 20:46:16 +0600

    categories updated

--- index.js
+++ index.js
@@ -30,62 +30,79 @@ async function run() {
 
     const closetCollection = client.db('closetWebsite').collection('closets');
     //post request to add closet
-app.post("/closets",async(req,res)=>{
-    const closet = req.body;
-    const result = await closetCollection.insertOne(closet);
-    res.send(result);
-})
+    app.post("/closets", async (req, res) => {
+      const closet = req.body;
+      const result = await closetCollection.insertOne(closet);
+      res.send(result);
+    })
 
-app.get("/closets",async(req,res)=>{
-    const limit = parseInt(req.query.limit);
-    const cursor = closetCollection.find();
-    if(limit){
+    app.get("/closets", async (req, res) => {
+      const limit = parseInt(req.query.limit);
+      const cursor = closetCollection.find();
+      if (limit) {
         const result = await cursor.limit(limit).toArray();
         res.send(result);
-    }
-    else{
+      }
+      else {
         const result = await cursor.toArray();
         res.send(result);
-    }
-})
+      }
+    })
 
-app.get("/closets/:id",async(req,res)=>{
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)}
-  const result = await closetCollection.findOne(query);
-  res.send(result);
-})
+    app.get("/closets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await closetCollection.findOne(query);
+      res.send(result);
+    })
 
 
-app.delete("/closets/:id",async(req,res)=>{
-  const id = req.params.id;
-  const query = {_id:new ObjectId(id)};
-  res.send(await closetCollection.deleteOne(query));
-})
+    app.delete("/closets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      res.send(await closetCollection.deleteOne(query));
+    })
 
-app.put("/closets/:id",async(req,res)=>{
-const id = req.params.id;
-const filter = {_id:new ObjectId(id)};
-const options = {upsert:true};
-const product = req.body
-const updatedCloset = {
-  $set:{
-    name: product.name,
-      category: product.category,
-      subCategory: product.subCategory,
-      gender: product.gender,
-      price: product.price,
-      sizes: product.sizes,
-      color: product.color,
-      shortDescription: product.shortDescription,
-      description: product.description,
-      imageUrl: product.imageUrl,
-      productDetails: product.productDetails,
-  }
-}
-const result = await closetCollection.updateOne(filter,updatedCloset,options)
-res.send(result);
-})
+    app.put("/closets/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const product = req.body
+      const updatedCloset = {
+        $set: {
+          name: product.name,
+          category: product.category,
+          subCategory: product.subCategory,
+          gender: product.gender,
+          price: product.price,
+          sizes: product.sizes,
+          color: product.color,
+          shortDescription: product.shortDescription,
+          description: product.description,
+          imageUrl: product.imageUrl,
+          productDetails: product.productDetails,
+        }
+      }
+      const result = await closetCollection.updateOne(filter, updatedCloset, options)
+      res.send(result);
+    })
+
+    // filter by category
+    app.get("/closets/category/:category", async (req, res) => {
+      const category = req.params.category;
+      let result;
+
+      if (category === "allproducts") {
+        result = await closetCollection.find().toArray();
+      }
+      else {
+        result = await closetCollection.find({
+          category: { $regex: `^${category}$`, $options: "i" }
+        }).toArray();
+      }
+      res.send(result);
+    })
+
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -97,9 +114,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Server is for closet vogue Dipta');
+  res.send('Server is for closet vogue Dipta');
 })
 
 app.listen(port, () => {
-    console.log(`lsitening on port ${port}`)
+  console.log(`lsitening on port ${port}`)
 })
